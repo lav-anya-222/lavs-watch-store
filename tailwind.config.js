7b4db30177dc14d8e0e09c3c513eec2d63563a34
lav-anya-222 <lavanya.ofzl@gmail.com>
2025-11-30 21:43:14 +0530

    Updated color scheme to new palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#000000',    // Your primary color
-        secondary: '#666666',  // Your secondary color  
-        background: '#ffffff', // Your background color
-        muted: '#e5e5e5',      // Your muted/border color
-      }
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: '#f8f8f7',
+                muted: '#cbc0b2',
+                primary: '#550b14',
+                secondary: '#7e6961',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
